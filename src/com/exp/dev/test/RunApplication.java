7643caeb2bea4d20a888ac2bbad3639ca928b06a
wns6440 <wns6440@gmail.com>
2021-11-03 10:45:26 +0900

    테스트 커밋 1

---
+++ src/com/exp/dev/test/RunApplication.java
@@ -0,0 +1,11 @@
+package com.exp.dev.test;
+
+public class RunApplication {
+
+	public static void main(String[] args) {
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+	}
+	
+}
